74da4b7882b82e57a282f42afc6652e53045b996
iuytrfghbjnkml <96704630+iuytrfghbjnkml@users.noreply.github.com>
2021-12-27 02:56:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ async def main():
             await check_changes()
             await sync_from_db()
         except Exception as e:
-            LOGGER.error(f"Errors occured while setting up database for DigiGram24, check the value of DATABASE_URI. Full error - {str(e)}", exc_info=True)
-            Config.STARTUP_ERROR="Errors occured while setting up database for DigiGram24, check the value of DATABASE_URI. Full error - {str(e)}"
+            LOGGER.error(f"Errors occured while setting up database for OnlineMusic24, check the value of DATABASE_URI. Full error - {str(e)}", exc_info=True)
+            Config.STARTUP_ERROR="Errors occured while setting up database for OnlineMusic24, check the value of DATABASE_URI. Full error - {str(e)}"
             LOGGER.info("Activating debug mode, you can reconfigure your bot with /env command.")
             await bot.stop()
             from utils import debug
